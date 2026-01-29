36d968f512f75780ad504974d20883a58f5ba6be
Molly Olson <molly.olson@student.centralia.edu>
2026-01-29 09:45:09 -0800

    This is throwing an exception error as it cannot load the json file unfortunately

--- FoodTruckW3Take2Or3Or4lol/FoodTruckLesDeux/FoodTruckLesDeux/Controllers/MenuController.cs
+++ FoodTruckW3Take2Or3Or4lol/FoodTruckLesDeux/FoodTruckLesDeux/Controllers/MenuController.cs
@@ -6,21 +6,21 @@ namespace FoodTruckLesDeux.Controllers
 {
     public class MenuController : Controller
     {
-        private readonly FoodTruckContext _contex;
+        private readonly FoodTruckContext _context;
         //compiler added below lines as possible fix for below _context.MenuItem error however it did not fix it
         //public List<MenuItem> item;
         //private object _context;
 
-        public MenuController(FoodTruckContext contex)
+        public MenuController(FoodTruckContext context)
         {
-            _contex = contex;
+            _context = context;
         }
         public IActionResult Index()
         {
-            var menu = _context.MenuItem.ToList();
+            var menu = _context.MenuItems.ToList();
             var vm = new MenuListViewModel
             {
-                Item = item,
+                Item = menu,
                 MenuTitle = "Available Menu Items",
                 TotalCount = menu.Count,
                 EmptyMessage = "No menu items currently available."
